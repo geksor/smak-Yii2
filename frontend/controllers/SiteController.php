a3b5d2b15e4d5d425f2f82939f371b2a65842ac9
geksor <geksor@gmail.com>
2018-08-31 19:41:08 +0300

    createUserAdd

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -75,4 +75,20 @@ class SiteController extends Controller
         ]);
     }
 
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+
 }
